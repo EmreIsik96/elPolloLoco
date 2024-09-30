7d412a6a1ad6252ea5702e774d3e37ebce60873f
Emre Isik <emre-isik@web.de>
2024-09-30 11:36:11 +0200

    add and fix bugs on responsive modus for mobile

--- js/game.js
+++ js/game.js
@@ -1,50 +1,9 @@
 let canvas;
 let world;
-let keyboard = new Keyboard();
+
 
 function init() {
-  canvas = document.getElementById("canvas");
+  canvas = document.getElementById("canvas"); 
+  let keyboard = new Keyboard();
   world = new World(canvas, keyboard);
 }
-
-window.addEventListener("keydown", (event) => {
-  if ((event.code == "ArrowUp")) {
-    keyboard.UP = true;
-  }
-  if ((event.code == "ArrowDown")) {
-    keyboard.DOWN = true;
-  }
-  if ((event.code == "ArrowRight")) {
-    keyboard.RIGHT = true;
-  }
-  if ((event.code == "ArrowLeft")) {
-    keyboard.LEFT = true;
-  }
-  if ((event.code == "Space")) {
-    keyboard.SPACE = true;
-  }
-  if ((event.code == "KeyF")) {
-    keyboard.F = true;
-  }    
-});
-
-window.addEventListener("keyup", (event) => {  
-    if ((event.code == "ArrowUp")) {
-      keyboard.UP = false;
-    }
-    if ((event.code == "ArrowDown")) {
-      keyboard.DOWN = false;
-    }
-    if ((event.code == "ArrowRight")) {
-      keyboard.RIGHT = false;
-    }
-    if ((event.code == "ArrowLeft")) {
-      keyboard.LEFT = false;
-    }
-    if ((event.code == "Space")) {
-      keyboard.SPACE = false;
-    }
-    if ((event.code == "KeyF")) {
-      keyboard.F = false;
-    }    
-  });
--- models/keyboard.class.js
+++ models/keyboard.class.js
@@ -1,9 +1,108 @@
-class Keyboard
-{
-    LEFT = false;
-    RIGHT = false;
-    UP = false;
-    DOWN = false;
-    SPACE = false;
-    F = false;
+class Keyboard {
+  LEFT = false;
+  RIGHT = false;
+  UP = false;
+  SPACE = false;
+  F = false;
+
+  /**
+   * Creates an instance of the Keyboard class.
+   * Initializes keyboard and mobile button event listeners.
+   *
+   * @constructor
+   */
+  constructor() {
+    this.keyboardEvents();
+    this.mobileButtonEvents();
+  }
+
+  /**
+   * Sets up keyboard events for key presses and releases.
+   */
+  keyboardEvents() {
+    window.addEventListener("keydown", (event) => {
+      if (event.code == "ArrowUp") {
+        this.UP = true;
+      }
+      if (event.code == "ArrowRight") {
+        this.RIGHT = true;
+      }
+      if (event.code == "ArrowLeft") {
+        this.LEFT = true;
+      }
+      if (event.code == "Space") {
+        this.SPACE = true;
+      }
+      if (event.code == "KeyF") {
+        this.F = true;
+      }
+    });
+
+    window.addEventListener("keyup", (event) => {
+      if (event.code == "ArrowUp") {
+        this.UP = false;
+      }
+      if (event.code == "ArrowDown") {
+        this.DOWN = false;
+      }
+      if (event.code == "ArrowRight") {
+        this.RIGHT = false;
+      }
+      if (event.code == "ArrowLeft") {
+        this.LEFT = false;
+      }
+      if (event.code == "Space") {
+        this.SPACE = false;
+      }
+      if (event.code == "KeyF") {
+        this.F = false;
+      }
+    });
+  }
+
+  mobileButtonEvents() {
+    document
+      .getElementById("left_btn")
+      .addEventListener("touchstart", (event) => {
+        event.preventDefault();
+        this.LEFT = true;
+        console.log("Left button pressed");
+      });
+    document
+      .getElementById("left_btn")
+      .addEventListener("touchend", (event) => {
+        event.preventDefault();
+        this.LEFT = false;
+      });
+    document
+      .getElementById("right_btn")
+      .addEventListener("touchstart", (event) => {
+        event.preventDefault();
+        this.RIGHT = true;
+      });
+    document
+      .getElementById("right_btn")
+      .addEventListener("touchend", (event) => {
+        event.preventDefault();
+        this.RIGHT = false;
+      });
+    document
+      .getElementById("up_btn")
+      .addEventListener("touchstart", (event) => {
+        event.preventDefault();
+        this.SPACE = true;
+      });
+    document.getElementById("up_btn").addEventListener("touchend", (event) => {
+      event.preventDefault();
+      this.SPACE = false;
+    });
+    document.getElementById("throw").addEventListener("touchstart", (event) => {
+      event.preventDefault();
+      this.F = true;
+    });
+    document.getElementById("throw").addEventListener("touchend", (event) => {
+      event.preventDefault();
+      this.F = false;
+    });
+  }
 }
